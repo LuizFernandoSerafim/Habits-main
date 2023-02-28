d5f4a04b9e23fd2e1d9112c3a3ae6cdeafc079b5
Luiz Fernando Silva Serafim <91030510+LuizFernandoSerafim@users.noreply.github.com>
2023-02-28 20:44:12 -0300

    Add files via upload

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Habit } from "./components/Habit";
+import './styles/global.css'
+function App() {
+  return (
+    <>
+   <Habit completed={4}/>
+   <Habit completed={3}/>
+   <Habit completed={2}/>
+   <Habit completed={1}/>
+   <Habit completed={0}/>
+    </>
+  )
+}
+export default App
